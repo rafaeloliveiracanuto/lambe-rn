32d3d7aeb0ab6237956642fe8eeeee3fd1157e40
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-23 10:34:12 -0300

    Add JSX

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,9 @@ import { StyleSheet, View, Image, Dimensions } from 'react-native'
 class Post extends Component {
     render() {
         return (
-            
+            <View style={styles.container}>
+                <Image source={this.props.image} style={styles.image} />
+            </View>
         )
     }
 }
