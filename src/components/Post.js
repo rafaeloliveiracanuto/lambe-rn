4f3310b894e719259edaa2a7b9a17caeb831c324
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-23 10:36:22 -0300

    Add styles

--- src/components/Post.js
+++ src/components/Post.js
@@ -10,3 +10,14 @@ class Post extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    image: {
+        width: Dimensions.get('window').width, 
+        height: Dimensions.get('window').width * 3 / 4,
+        resizeMode: 'contain'    
+    }
+})
