7a8e3094a2e98aa9e20118dbf0fe537ab1b557c2
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-23 09:39:21 -0300

    Add styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,9 +14,35 @@ class Header extends Component {
             <View style={styles.container}>
                 <View style={styles.rowContainer}>
                     <Image source={icon} style={styles.image} />
-                    <Text style={styles.title}></Text>
+                    <Text style={styles.title}>Social Up!</Text>
                 </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: Platform.OS === 'ios' ? 20 : 0,
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: '#BBB'
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    image: {
+        height: 30,
+        width: 30,
+        resizeMode: 'contain'
+    },
+    title: {
+        color: '#000',
+        fontFamily: 'shelter',
+        height: 30,
+        fontSize: 28
+    }
+})
+
+
