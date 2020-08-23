91aeef349d084bbc1df752fa565c1d9563a0ac7d
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-23 10:32:53 -0300

    Create Post class

---
+++ src/components/Post.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Image, Dimensions } from 'react-native'
+
+class Post extends Component {
+    render() {
+        return (
+            
+        )
+    }
+}
