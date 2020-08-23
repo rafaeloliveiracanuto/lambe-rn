42471a580e7164e78db76ec91edf440bc4ba7b80
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-23 10:48:46 -0300

    Add styles

--- src/components/Author.js
+++ src/components/Author.js
@@ -11,3 +11,22 @@ export default props => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    avatar: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        marginHorizontal: 10,
+    },
+    nickname: {
+        color: '#444',
+        marginVertical: 10,
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
+})
