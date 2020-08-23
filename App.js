9b72010aeee9437bbdb29a4926f9955f301f4c74
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-23 09:45:14 -0300

    Put Header

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import Header from './src/components/Header'
 
 export default class App extends Component {
     render() {
         return (
-  
+            <Header />
         )
     }
 }
