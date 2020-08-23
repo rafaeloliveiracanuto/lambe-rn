3ebce79670a03f1e0c9a97d506eaaeac006200f0
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-23 09:33:44 -0300

    Add JSX in render function

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,12 @@ import icon from '../../assets/imgs/icon.png'
 class Header extends Component {
     render() {
         return (
-            
+            <View style={styles.container}>
+                <View style={styles.rowContainer}>
+                    <Image source={icon} style={styles.image} />
+                    <Text style={styles.title}></Text>
+                </View>
+            </View>
         )
     }
 }
