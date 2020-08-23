1f7465b105efa60afab118db70a3f1e4ff147e2e
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-23 10:45:53 -0300

    Add JSX

--- src/components/Author.js
+++ src/components/Author.js
@@ -2,3 +2,12 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { Gravatar } from 'react-native-gravatar'
 
+export default props => {
+    return (
+        <View style={styles.container}>
+            <Gravatar options={{ email: props.email, secure: true }}
+                style={styles.avatar} />
+            <Text style={styles.nickname}>{props.nickname}</Text>
+        </View>
+    )
+}
