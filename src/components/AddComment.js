c78543035cc628fb2b7ee6ce2f267b36415c35cb
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-24 11:47:00 -0300

    Add function handleAddComment

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -9,3 +9,13 @@ import {
 } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
+class AddComment extends Component {
+    state = {
+        comment: '',
+        askComment: true,
+    }
+
+    handleAddComment = () => {
+        Alert.alert('Added!', this.state.comment)
+    }
+}
