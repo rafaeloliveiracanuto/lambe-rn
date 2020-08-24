ed32dd5ff6f01ac3c7a06843ba85217fb5fb2e2e
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-23 22:37:47 -0300

    Add styles

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -28,3 +28,22 @@ class Comments extends Component {
         )
     }
 }
+
+const styles  = StyleSheet.create({
+    container: {
+        flex: 1,
+        margin: 10
+    },
+    commentContainer: {
+        flexDirection: 'row',
+        marginTop: 5
+    },
+    nickname: {
+        marginLeft: 5,
+        fontWeight: 'bold',
+        color: '#444'
+    },
+    comment: {
+        color: '#555'
+    }
+})
