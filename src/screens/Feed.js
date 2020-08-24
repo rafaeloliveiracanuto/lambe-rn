21b4e29a2fcbbf43fdfab2e7c6ddfe475f97e5c5
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-24 14:15:18 -0300

    Create class Feed

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -3,3 +3,11 @@ import { StyleSheet, FlatList, View } from 'react-native'
 import Header from '../components/Header'
 import Post from '../components/Post'
 
+class Feed extends Component {
+    state = {
+        posts: [{
+            id: Math.random(),
+            nic
+        }]
+    }
+}
