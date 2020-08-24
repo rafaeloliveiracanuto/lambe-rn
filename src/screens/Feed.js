242ba84d3e2fda784e6b2571ce82e711645d84f6
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-24 14:30:32 -0300

    Add styles

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -38,3 +38,13 @@ class Feed extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    }
+})
+
