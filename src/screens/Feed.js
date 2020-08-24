d1e0a52da23c70db25eef79765419792db335db0
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-24 14:14:15 -0300

    Import components

---
+++ src/screens/Feed.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react'
+import { StyleSheet, FlatList, View } from 'react-native'
+import Header from '../components/Header'
+import Post from '../components/Post'
+
