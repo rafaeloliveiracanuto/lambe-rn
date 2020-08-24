f4928a0a24712cbebd8ac166afa6c2388f16687c
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-23 22:39:48 -0300

    Add Comments within Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Image, Dimensions } from 'react-native'
 import Author from './Author'
+import Comments from './Comments'
 
 class Post extends Component {
     render() {
@@ -8,6 +9,7 @@ class Post extends Component {
             <View style={styles.container}>
                 <Image source={this.props.image} style={styles.image} />
                 <Author email='rafael@gmail.com' nickname='Rafael Canuto' />
+                <Comments comments={this.props.comments} />
             </View>
         )
     }
