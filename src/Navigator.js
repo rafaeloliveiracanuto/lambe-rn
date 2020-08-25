326f5488c2159d99738559c094e22fde736b76ae
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-25 09:18:20 -0300

    Add MenuRoutes object

--- src/Navigator.js
+++ src/Navigator.js
@@ -3,3 +3,14 @@ import { createBottomNavigator } from 'react-navigation'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import Feed from './screens/Feed'
 
+const MenuRoutes = {
+    Feed: {
+        name: 'Feed',
+        screen: Feed,
+        navigationOptions: {
+            title: 'Feed',
+            tabBarIcon: ({ tintColor }) => 
+                <Icon name='home' size={30} color={tintColor} />
+        }
+    },
+}
