889c4700c330c4dd77292231106e764d3f0959e6
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-25 11:27:43 -0300

    Import components

---
+++ src/screens/AddPhoto.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {
+    View, 
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TextInput,
+    Image,
+    Dimensions,
+    Platform,
+    ScrollView,
+    Alert
+} from 'react-native'
+import ImagePicker from 'react-native-image-picker'
+
