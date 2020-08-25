444b7e52f1a4ceeda8ab9446377e2f580bc2061e
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-25 11:53:45 -0300

    Finish styles

--- src/screens/AddPhoto.js
+++ src/screens/AddPhoto.js
@@ -59,5 +59,38 @@ class AddPhoto extends Component {
 }
 
 const styles = StyleSheet.create({
-        
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginTop: Platform.OS === 'ios' ? 30 : 10,
+        fontWeight: 'bold'
+    },
+    imageContainer: {
+        width: '90%',
+        height: Dimensions.get('window').width * 3 / 4,
+        backgroundColor: '#EEE',
+        marginTop: 10
+    },
+    image: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').width * 3 / 4,
+        resizeMode: 'center'
+    },
+    button: {
+        marginTop: 30,
+        padding: 10,
+        backgroundColor: '#4286f4'
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#FFF'
+    },
+    input: {
+        marginTop: 20,
+        width: '90%'
+    }
 })
+
