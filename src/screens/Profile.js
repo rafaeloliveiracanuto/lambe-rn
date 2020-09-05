81609b51b5935f37831d1c393681db29b0d61b43
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-04 22:22:57 -0300

    Add styles

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -24,6 +24,31 @@ class Profile extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        
+        flex: 1,
+        alignItems: 'center'
+    },
+    avatar: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        marginTop: 100,
+    },
+    nickname: {
+        marginTop: 30,
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+    email: {
+        marginTop: 20,
+        fontSize: 25
+    },
+    button: {
+        marginTop: 30,
+        padding: 10,
+        backgroundColor: '#4286f4'
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#FFF'
     }
 })
