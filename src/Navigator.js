671b4c9b61c37fb44f30b7695406c4fadf738109
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-05 14:37:36 -0300

    Import more components

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { createBottomNavigator, createBottomTabNavigator } from 'react-navigation'
+import { 
+    createBottomTabNavigator,
+    createSwitchNavigator
+} from 'react-navigation'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
 import AddPhoto from './screens/AddPhoto'
 import Profile from './screens/Profile'
+import Login from './screens/Login'
 
 const MenuRoutes = {
     Feed: {
