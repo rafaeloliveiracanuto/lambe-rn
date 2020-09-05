8c2ce9f247eaa687e6bba437d7fcc1b9bb38ad9a
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-04 22:12:02 -0300

    Import components and create class Profile

---
+++ src/screens/Profile.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { Gravatar } from 'react-native-gravatar'
+
+class Profile extends Component {
+    
+}
