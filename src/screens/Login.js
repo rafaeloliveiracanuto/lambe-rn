31f24eb82fa6dd91b14fa22437c5ba20fadfbb2e
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-05 14:16:33 -0300

    Import components and create class Login

---
+++ src/screens/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TextInput
+} from 'react-native'
+
+class Login extends Component {
+    
+}
