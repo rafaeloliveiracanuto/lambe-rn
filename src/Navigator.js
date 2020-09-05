4c1d1907eff43a86c5f17afcb046412f3e456af4
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-05 14:39:36 -0300

    Add loginOrProfileRouter

--- src/Navigator.js
+++ src/Navigator.js
@@ -10,6 +10,13 @@ import AddPhoto from './screens/AddPhoto'
 import Profile from './screens/Profile'
 import Login from './screens/Login'
 
+const loginOrProfileRouter = createSwitchNavigator({
+    Profile: Profile,
+    Auth: Login
+}, {
+    initialRouteName: 'Profile'
+})
+
 const MenuRoutes = {
     Feed: {
         name: 'Feed',
@@ -31,7 +38,7 @@ const MenuRoutes = {
     },
     Profile: {
         name: 'Profile',
-        screen: Profile,
+        screen: loginOrProfileRouter,
         navigationOptions: {
             title: 'Profile',
             tabBarIcon: ({ tintColor }) =>
