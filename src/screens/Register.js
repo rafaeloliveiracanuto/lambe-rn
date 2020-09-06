a9983a71098a40b70b882fae0cf3e8f4658398cf
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-06 09:35:47 -0300

    Add styles

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -33,3 +33,29 @@ class Register extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    button: {
+        marginTop: 30,
+        padding: 10,
+        backgroundColor: '#4286f4'
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#FFF'
+    },
+    input: {
+        marginTop: 20,
+        width: '90%',
+        backgroundColor: '#EEE',
+        height: 40,
+        borderWidth: 1,
+        borderColor: '#333',
+        paddingLeft: 15
+    }
+})
