a6c7ece6d5f12c9aa11a7500b96cf92f03e2d8da
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-06 09:27:11 -0300

    Add TextInputs

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -13,4 +13,20 @@ class Register extends Component {
         email: '',
         password: ''
     }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput placeholder='Name' style={styles.input}
+                    autoFocus={true} value={this.state.name}
+                    onChangeText={name => this.setState({ name })} />
+                <TextInput placeholder='Email' style={styles.input}
+                    keyboardType='email-address' value={this.state.email}
+                    onChangeText={email => this.setState({ email })} />
+                <TextInput placeholder='Password' style={styles.input}
+                    secureTextEntry={true} value={this.state.password}
+                    onChangeText={password => this.setState({ password })} />
+            </View>
+        )
+    }
 }
