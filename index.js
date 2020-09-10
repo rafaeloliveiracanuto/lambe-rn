b5456303bfde574918efd52780d8180c0acdfc0c
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-10 14:33:07 -0300

    Import components

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import React from 'react'
+import { Provider } from 'react-redux'
 import {AppRegistry} from 'react-native'
 import {name as appName} from './app.json'
 import Navigator from './src/Navigator'
+
 
 AppRegistry.registerComponent(appName, () => Navigator)
