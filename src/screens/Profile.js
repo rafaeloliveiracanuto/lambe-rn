5f86db0a7c46825be5e11848ca7b24d8c8cf4855
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-12 14:10:47 -0300

    Add properties from global state

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -6,16 +6,17 @@ import { Gravatar } from 'react-native-gravatar'
 
 class Profile extends Component {
     logout = () => {
+        this.props.onLogout()
         this.props.navigation.navigate('Auth')
     }
 
     render() {
-        const options = { email: 'rafael@gmail.com', secure: true }
+        const options = { email: this.props.email, secure: true }
         return (
             <View style={styles.container}>
                 <Gravatar options={options} style={styles.avatar} />
-                <Text style={styles.nickname}>Rafael Creator</Text>
-                <Text style={styles.email}>rafael@gmail.com</Text>
+                <Text style={styles.nickname}>{this.props.name}</Text>
+                <Text style={styles.email}>{this.props.email}</Text>
                 <TouchableOpacity onPress={this.logout} style={styles.button}>
                     <Text style={styles.buttonText}>Logout</Text>
                 </TouchableOpacity>
