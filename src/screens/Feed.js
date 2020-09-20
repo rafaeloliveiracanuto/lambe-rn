982f6f9e390e5e1c9d8a9729662e182acb12302a
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-19 23:29:08 -0300

    Remove state

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -4,28 +4,6 @@ import Header from '../components/Header'
 import Post from '../components/Post'
 
 class Feed extends Component {
-    state = {
-        posts: [{
-            id: Math.random(),
-            nickname: 'Economic Student',
-            email: 'economicStudent@gmail.com',
-            image: require('../../assets/imgs/fence.jpg'),
-            comments: [{
-                nickname: 'Ludwig von Mises',
-                comment: 'Very competitive!'
-            }, {
-                nickname: 'Karl Marx',
-                comment: 'Can you share this photo with me?'
-            }]
-        }, {
-            id: Math.random(),
-            nickname: 'Philosophy Student',
-            email: 'philosophyStudent@gmail.com',
-            image: require('../../assets/imgs/bw.jpg'),
-            comments: []
-        }]
-    }
-
     render() {
         return (
             <View style={styles.container}>
