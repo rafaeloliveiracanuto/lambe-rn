b76196c6a8f3766bad95f7d3cd7641a11a2fe046
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-19 23:28:39 -0300

    Add posts in the initialState

--- src/store/reducers/Posts.js
+++ src/store/reducers/Posts.js
@@ -1,5 +1,23 @@
 import { ADD_POST } from '../actions/actionTypes'
 
 const initialState = {
-    
+    posts: [{
+        id: Math.random(),
+        nickname: 'Economic Student',
+        email: 'economicStudent@gmail.com',
+        image: require('../../../assets/imgs/fence.jpg'),
+        comments: [{
+            nickname: 'Ludwig von Mises',
+            comment: 'Very competitive!'
+        }, {
+            nickname: 'Karl Marx',
+            comment: 'Can you share this photo with me?'
+        }]
+    }, {
+        id: Math.random(),
+        nickname: 'Philosophy Student',
+        email: 'philosophyStudent@gmail.com',
+        image: require('../../../assets/imgs/bw.jpg'),
+        comments: []
+    }]
 }
