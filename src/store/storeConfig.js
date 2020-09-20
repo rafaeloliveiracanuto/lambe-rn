12415182c5a4030b02d43c3ab0f808097991c120
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-19 23:36:29 -0300

    Add postsReducer in the reducers

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers } from 'redux'
 import userReducer from './reducers/User'
+import postsReducer from './reducers/Posts'
+
 
 const reducers = combineReducers({
-    user: userReducer,  
+    user: userReducer,
+    posts: postsReducer,  
 })
 
 const storeConfig = () => {
